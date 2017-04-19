299e8da8e5940b932f48fb5b590f61b8dfab88a9
Ian Zolitor <ianzolitor@gmail.com>
2017-04-19 17:50:01 -0400

    assignment 3 setup

--- script.js
+++ script.js
@@ -19,36 +19,54 @@
 // 	return this.currentValue;
 // }
 
-// Assignment 2
+// // Assignment 2
 
-function Photo (fileName, location){
-	this.fileName = fileName;
-	this.location = location;
+// function Photo (fileName, location){
+// 	this.fileName = fileName;
+// 	this.location = location;
+// }
+
+// var photo1 = new Photo("photo1.jpg", "Niagara Falls");
+// var photo2 = new Photo("photo2.jpg", "The Circus");
+// var photo3 = new Photo("photo3.jpg", "Music Festival");
+
+// var photos = [photo1, photo2, photo3];
+
+// function Album (){
+// 	this.gallery = [];
+// 	this.addPhoto = addPhoto;
+// 	this.listPhotos = listPhotos
+
+// }
+
+// var myAlbum = new Album();
+// var anotherAlbum = new Album();
+
+// function addPhoto(x){
+// 		this.gallery.push(x);
+// 	}
+
+
+// function listPhotos() {
+// 	for (var i = 0; i < this.gallery.length; i++) {
+// 		console.log(this.gallery[i].fileName)
+// 	}
+// }
+
+// Assignment 3
+
+function Person(name){
+	this.name = name
 }
 
-var photo1 = new Photo("photo1.jpg", "Niagara Falls");
-var photo2 = new Photo("photo2.jpg", "The Circus");
-var photo3 = new Photo("photo3.jpg", "Music Festival");
-
-var photos = [photo1, photo2, photo3];
-
-function Album (){
-	this.gallery = [];
-	this.addPhoto = addPhoto;
-	this.listPhotos = listPhotos
-
+function Student(goodStudent){
+	this.goodStudent = goodStudent
+	Person.apply(this, arguments);
+	var currentGrade = 100
 }
 
-var myAlbum = new Album();
-var anotherAlbum = new Album();
-
-function addPhoto(x){
-		this.gallery.push(x);
-	}
-
-
-function listPhotos() {
-	for (var i = 0; i < this.gallery.length; i++) {
-		console.log(this.gallery[i].fileName)
-	}
+function Teacher(goodTeacher){
+	this.goodTeacher = goodTeacher
+	Person.apply(this,arguments)
 }
+var ian = new Student(true, "Ian")
